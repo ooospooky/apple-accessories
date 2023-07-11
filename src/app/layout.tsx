37c76dee0eed7a2bde4c7a2c42b1515ac2c33e90
Johnny Tai <bm414148@gmail.com>
2023-07-11 11:48:58 +0800

    add roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Roboto } from 'next/font/google'
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+ })
 
 export const metadata: Metadata = {
   title: 'Apple Store',
@@ -16,7 +19,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+      <body className={roboto.className}>{children}</body>
     </html>
   )
 }
