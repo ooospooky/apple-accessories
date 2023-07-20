67166cc3b3e8395dc838e8f63bd8ed1de2fe2f42
Johnny Tai <bm414148@gmail.com>
2023-07-20 13:58:44 +0800

    change import products to allProducts

--- src/app/mac/page.tsx
+++ src/app/mac/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import {products} from '../components/products'
+import { allProducts } from '../components/allProducts'
 export default function Mac() {
   return (
     <div className='w-full'>
@@ -18,21 +18,21 @@ export default function Mac() {
       <h2 className="text-5xl font-semibold tracking-normal pb-16 pt-24 text-center" data-analytics-section="Mac 精選配件">Mac 精選配件</h2>
 
       {/* Porducts */}
-      <div  className='w-full px-40 '>
+      <div className='w-8/12 m-auto '>
         <ul className="w-full flex flex-wrap m-0 mb-20">
-          {products.map(({id,category,name,width,height,src,price})=>{
-            return(
-              <li key={id} className={`bg-[#f2f2f2] list-none overflow-hidden rounded-3xl border-4 border-solid border-white ${name === "Studio Display" ? "basis-2/3" : "basis-1/3" }`}>
-            <div >
-              <div className="photo px-16 py-0 ">
-                <Image  className="mx-auto mb-3.5 " src={src} width={width} height={height} alt={name} />
-              </div>
-              <div  className={` ${id === "1" ? "pt-16" : "" }`}>
-                <h3 className="text-center font-normal text-lg mb-10 ">{name}</h3>
-                <p className='text-center font-normal text-base mb-20'>{price}</p>
-              </div>
-            </div>
-          </li>
+          {allProducts.map(({ id, category, name, width, height, src, price }) => {
+            return (
+              <li key={id} className={`bg-[#f2f2f2] list-none overflow-hidden rounded-3xl border-4 border-solid border-white ${name === "Studio Display" ? "basis-2/3" : "basis-1/3"}`}>
+                <div >
+                  <div className="photo px-16 py-0 ">
+                    <Image className="mx-auto mb-3.5 " src={src} width={width} height={height} alt={name} />
+                  </div>
+                  <div className={` ${id === "1" ? "pt-16" : ""}`}>
+                    <h3 className="text-center font-normal text-lg mb-10 ">{name}</h3>
+                    <p className='text-center font-normal text-base mb-20'>{price}</p>
+                  </div>
+                </div>
+              </li>
             )
           })}
         </ul>
