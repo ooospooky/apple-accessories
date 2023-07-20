4d7729428b64285ba4a4ff3d9d6f68cec36dde71
Johnny Tai <bm414148@gmail.com>
2023-07-20 14:33:46 +0800

    import & use ProductList

--- src/app/mac/page.tsx
+++ src/app/mac/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { allProducts } from '../components/allProducts'
+import { ProductList } from '../components/ProductList'
 export default function Mac() {
   return (
     <div className='w-full'>
@@ -20,21 +21,7 @@ export default function Mac() {
       {/* Porducts */}
       <div className='w-8/12 m-auto '>
         <ul className="w-full flex flex-wrap m-0 mb-20">
-          {allProducts.map(({ id, category, name, width, height, src, price }) => {
-            return (
-              <li key={id} className={`bg-[#f2f2f2] list-none overflow-hidden rounded-3xl border-4 border-solid border-white ${name === "Studio Display" ? "basis-2/3" : "basis-1/3"}`}>
-                <div >
-                  <div className="photo px-16 py-0 ">
-                    <Image className="mx-auto mb-3.5 " src={src} width={width} height={height} alt={name} />
-                  </div>
-                  <div className={` ${id === "1" ? "pt-16" : ""}`}>
-                    <h3 className="text-center font-normal text-lg mb-10 ">{name}</h3>
-                    <p className='text-center font-normal text-base mb-20'>{price}</p>
-                  </div>
-                </div>
-              </li>
-            )
-          })}
+          < ProductList type='mac' />
         </ul>
       </div>
     </div>
