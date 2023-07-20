df691d4aaac57ed463e508a8b92e9dc50be3186c
Johnny Tai <bm414148@gmail.com>
2023-07-20 14:32:42 +0800

    basic Card done

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import Image from 'next/image'
-
-export const ProductCard = () => {
+import { IProduct } from './allProducts'
+interface IProductCard extends IProduct {
+  productIndex: number;
+}
+export const ProductCard: React.FC<IProductCard> = ({ id, category, name, width, height, src, price, productIndex }) => {
+  const formattedPrice = price.toLocaleString('zh-TW', {
+    style: 'currency',
+    currency: 'TWD',
+    minimumFractionDigits: 0,
+  });
   return (
-    <li key={id} className={`bg-[#f2f2f2] list-none overflow-hidden rounded-3xl border-4 border-solid border-white ${name === "Studio Display" ? "basis-2/3" : "basis-1/3"}`}>
+    <li key={id} className={`bg-[#f2f2f2] list-none overflow-hidden rounded-3xl border-4 border-solid border-white ${productIndex === 2 ? "basis-2/3" : "basis-1/3"}`}>
       <div >
         <div className="photo px-16 py-0 ">
           <Image className="mx-auto mb-3.5 " src={src} width={width} height={height} alt={name} />
         </div>
-        <div className={` ${id === "1" ? "pt-16" : ""}`}>
+        <div className={` ${productIndex === 1 ? "pt-16" : ""}`}>
           <h3 className="text-center font-normal text-lg mb-10 ">{name}</h3>
-          <p className='text-center font-normal text-base mb-20'>{price}</p>
+          <p className='text-center font-normal text-base mb-20'>NT{formattedPrice}</p>
         </div>
       </div>
     </li>
