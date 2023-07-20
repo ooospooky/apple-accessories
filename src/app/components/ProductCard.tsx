a975ab95eff230366a27728587b6d315e5cf57fd
Johnny Tai <bm414148@gmail.com>
2023-07-20 14:54:15 +0800

    justify-end for align text

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -11,12 +11,12 @@ export const ProductCard: React.FC<IProductCard> = ({ id, category, name, width,
     minimumFractionDigits: 0,
   });
   return (
-    <li key={id} className={`bg-[#f2f2f2] list-none overflow-hidden rounded-3xl border-4 border-solid border-white ${productIndex === 2 ? "basis-2/3" : "basis-1/3"}`}>
+    <li key={id} className={`bg-[#f2f2f2] list-none overflow-hidden rounded-3xl border-4 border-solid border-white ${productIndex === 2 ? "basis-2/3" : "basis-1/3"} flex flex-col justify-end`}>
       <div >
-        <div className="photo px-16 py-0 ">
+        <div className="photo px-16 py-0">
           <Image className="mx-auto mb-3.5 " src={src} width={width} height={height} alt={name} />
         </div>
-        <div className={` ${productIndex === 1 ? "pt-16" : ""}`}>
+        <div className={` ${productIndex === 1 ? "pt-16" : ""} `}>
           <h3 className="text-center font-normal text-lg mb-10 ">{name}</h3>
           <p className='text-center font-normal text-base mb-20'>NT{formattedPrice}</p>
         </div>
