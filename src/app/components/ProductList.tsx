cf96adeead8e74add0cc5314d26c0271378134ee
Johnny Tai <bm414148@gmail.com>
2023-07-20 14:33:14 +0800

    basic ProductList done

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -1,24 +1,16 @@
 import React from 'react'
-
-export const ProductList = () => {
+import { allProducts } from './allProducts'
+import { ProductCard } from './ProductCard'
+interface IproductList {
+  type: string
+}
+export const ProductList: React.FC<IproductList> = ({ type }) => {
   return (
     <div className='w-8/12 m-auto '>
       <ul className="w-full flex flex-wrap m-0 mb-20">
-        {/* {allProducts.map(({ id, category, name, width, height, src, price }) => {
-          return (
-            <li key={id} className={`bg-[#f2f2f2] list-none overflow-hidden rounded-3xl border-4 border-solid border-white ${name === "Studio Display" ? "basis-2/3" : "basis-1/3"}`}>
-              <div >
-                <div className="photo px-16 py-0 ">
-                  <Image className="mx-auto mb-3.5 " src={src} width={width} height={height} alt={name} />
-                </div>
-                <div className={` ${id === "1" ? "pt-16" : ""}`}>
-                  <h3 className="text-center font-normal text-lg mb-10 ">{name}</h3>
-                  <p className='text-center font-normal text-base mb-20'>{price}</p>
-                </div>
-              </div>
-            </li>
-          )
-        })} */}
+        {allProducts.map((product, index) => (
+          <ProductCard key={index} {...product} productIndex={index + 1} />
+        ))}
       </ul>
     </div>
   )
