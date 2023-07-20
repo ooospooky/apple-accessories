35f1cb9ef205a4c5315b9dce35193a81adcae9d4
Johnny Tai <bm414148@gmail.com>
2023-07-20 17:40:21 +0800

    improve home page headblock IMG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,17 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="containter  ">
-      <div className='headblock w-full h-full flex flex-row bg-[#f0eff2] '  >
+      <div className='headblock w-full h-full flex flex-row bg-[#f5f5f7] px-80'  >
         <div className="info basis-1/2 flex flex-col justify-center items-center  gap-12" >
-          <h2 className="text-5xl">MagSafe。混‍搭，很⁠搭。</h2>
-          <p className='text-2xl'>保護殼、卡套、無線充電器，或行動電源，全都可貼合。</p>
+          <h2 className="text-7xl font-semibold mx-1 leading-tight">MagSafe。混‍搭，很⁠搭。</h2>
+          <p className='text-3xl'>保護殼、卡套、無線充電器，或行動電源，全都可貼合。</p>
         </div>
-        <div className='basis-1/2 ' >
-          <Image src="/apple-homepage.jpg" alt="Homepage-IMG" height={500} width={500} />
+        <div className='basis-1/2 -ml-8' >
+          <Image src="/apple-homepageIMG.png" alt="Homepage-IMG" height={300} width={900} />
         </div>
       </div>
       <div className='mx-96'>
-        <div className="borderline my-20 border-t border-solid border-[#d2d2d7] "></div>
+        <div className="borderline my-20 border-t border-solid border-[#f5f5f7] "></div>
 
         <div className="container w-full h-full mt-8 ">
           <ul className="m-0 p-0 text-center flex justify-around ">
