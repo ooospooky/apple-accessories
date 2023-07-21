3caddf227b2efbb753076f2ce5d55ed7c8bcf051
Johnny Tai <bm414148@gmail.com>
2023-07-21 18:49:00 +0800

    add link to '/product/{id}

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { IProduct } from './allProducts'
+import Link from 'next/link';
 
 export const ProductCard: React.FC<IProduct> = ({ id, category, name, width, height, src, price }) => {
   const formattedPrice = price.toLocaleString('zh-TW', {
@@ -9,14 +10,18 @@ export const ProductCard: React.FC<IProduct> = ({ id, category, name, width, hei
     minimumFractionDigits: 0,
   });
   return (
-    <li key={id} className={`bg-[#f2f2f2] list-none overflow-hidden rounded-3xl border-4 border-solid border-white  flex flex-col justify-end ${height === 465 ? "basis-2/3" : "basis-1/3"}`}>
-      <div className="photo px-16 m-auto ">
-        <Image className="mx-auto mb-3.5 " src={src} width={width} height={height} alt={name} />
-      </div>
-      <div>
-        <h3 className="text-center font-semibold text-lg mb-10 ">{name}</h3>
-        <p className='text-center font-normal text-base mb-20'>NT{formattedPrice}</p>
-      </div>
+
+    <li key={id} className={`bg-[#f2f2f2] list-none overflow-hidden rounded-3xl border-4 border-solid border-white  flex flex-col justify-end ${height === 465 ? "basis-2/3" : "basis-1/3"}`} >
+      <Link href={`/product/${id}`}>
+        <div className="photo px-16 m-auto ">
+          <Image className="mx-auto mb-3.5 " src={src} width={width} height={height} alt={name} />
+        </div>
+        <div>
+          <h3 className="text-center font-semibold text-lg mb-10 ">{name}</h3>
+          <p className='text-center font-normal text-base mb-20'>NT{formattedPrice}</p>
+        </div>
+      </Link> 
+
     </li>
   )
 }
