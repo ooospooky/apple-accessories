b3b2e2c837d66ca15b2ca70e51823881ffb3c38c
Johnny Tai <bm414148@gmail.com>
2023-07-27 17:12:20 +0800

    import ProductInfoAndCompatibility

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -2,23 +2,24 @@
 import React, { useState } from 'react'
 import { allProducts, IProduct } from '@/app/components/allProducts'
 import Image from 'next/image';
-import getNextTwoDays from '@/app/helper/getNextTwoDays';
 import { OrderTimeAndDate } from '@/app/components/OrderTimeAndDate';
-import { AddToCartBtn } from '@/app/components/addToCartBtn';
+import { AddToCartBtn } from '@/app/components/AddToCartBtn';
 import { CollectionInfo } from '@/app/components/CollectionInfo';
 import { PurchaseAssistance } from '@/app/components/PurchaseAssistance';
 import { PriceInfo } from '@/app/components/PriceInfo';
+import { ProductInfoAndCompatibility } from '@/app/components/ProductInfoAndCompatibility';
 
 
 export default function ProductId({ params }: { params: { productId: string } }) {
   const foundProduct = allProducts.find((item) => item.id === params.productId);
-  const { id, category, name, width, height, src, price }: IProduct = foundProduct || {} as IProduct;
+  const { id, category, name, width, height, src, price, productInfo, compatibilityInfo }: IProduct = foundProduct || {} as IProduct;
 
 
 
   return (
+
     <div className="w-full h-full   ">
-      <div className="w-9/12 h-full my-12 mx-auto flex flex-row">
+      <div className="w-7/12 h-full my-12 mx-auto flex flex-row">
         <div className="basis-1/3 flex flex-col gap-12">
           <h2 className="text-6xl font-semibold leading-tight ">{name}</h2>
           <PriceInfo price={price} />
@@ -79,7 +80,13 @@ export default function ProductId({ params }: { params: { productId: string } })
             <Image src={src} layout="responsive" height={1} width={1} alt='' className='object-fill m-auto max-w-5xl max-h-5xl' />
           </div>
         </div>
+
+
       </div>
+      <section className="w-7/12 h-full my-12 mx-auto flex flex-col">
+        <ProductInfoAndCompatibility productInfo={productInfo} compatibilityInfo={compatibilityInfo} />
+      </section>
     </div>
   )
 }
+
