97c6e48fd3094a3be54af80043ed5b6d003ace2f
Johnny Tai <bm414148@gmail.com>
2023-07-30 15:05:26 +0800

    import ColorSelect.tsx & create useState of selectedColor

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -8,11 +8,13 @@ import { CollectionInfo } from '@/app/components/CollectionInfo';
 import { PurchaseAssistance } from '@/app/components/PurchaseAssistance';
 import { PriceInfo } from '@/app/components/PriceInfo';
 import { ProductInfoAndCompatibility } from '@/app/components/ProductInfoAndCompatibility';
+import { ColorSelect } from '@/app/components/ColorSelect';
 
 
 export default function ProductId({ params }: { params: { productId: string } }) {
+  const [selectedColor, setSelectedColor] = useState<string | null>(null);
   const foundProduct = allProducts.find((item) => item.id === params.productId);
-  const { id, category, name, width, height, src, price, productInfo, compatibilityInfo }: IProduct = foundProduct || {} as IProduct;
+  const { id, category, name, width, height, src, price, colorsType, colors, productInfo, compatibilityInfo }: IProduct = foundProduct || {} as IProduct;
 
 
 
@@ -21,53 +23,9 @@ export default function ProductId({ params }: { params: { productId: string } })
     <div className="w-full h-full   ">
       <div className="w-7/12 h-full my-12 mx-auto flex flex-row">
         <div className="basis-1/3 flex flex-col gap-12">
-          <h2 className="text-6xl font-semibold leading-tight ">{name}</h2>
+          <h2 className="text-6xl font-semibold leading-tight ">{name} -{selectedColor}</h2>
           <PriceInfo price={price} />
-          <div>
-            <h2 className="">顏色 - 墨水色</h2>
-            <ul className='flex flex-row gap-6 my-4'>
-              <li>
-                <label className="flex items-center cursor-pointer">
-                  <input
-                    type="radio"
-                    name="color"
-                    value="pink"
-                    className="hidden peer/darkred"
-                  />
-                  <div
-                    className="w-16 h-16 rounded-full bg-pink-800 peer-checked/darkred:ring peer-checked/darkred:ring-blue-500 peer-checked/darkred:ring-offset-4"
-                  ></div>
-                </label>
-
-              </li>
-              <li>
-                <label className="flex items-center cursor-pointer">
-                  <input
-                    type="radio"
-                    name="color"
-                    value="blue"
-                    className="hidden peer/blue"
-                  />
-                  <div
-                    className="w-16 h-16 rounded-full bg-blue-500 peer-checked/blue:ring peer-checked/blue:ring-blue-500 peer-checked/blue:ring-offset-4"
-                  ></div>
-                </label>
-              </li>
-              <li>
-                <label className="flex items-center cursor-pointer">
-                  <input
-                    type="radio"
-                    name="color"
-                    value="pink"
-                    className="hidden peer/pink"
-                  />
-                  <div
-                    className="w-16 h-16 rounded-full bg-pink-500 peer-checked/pink:ring peer-checked/pink:ring-blue-500 peer-checked/pink:ring-offset-4"
-                  ></div>
-                </label>
-              </li>
-            </ul>
-          </div>
+          {colors && colorsType && <ColorSelect colors={colors} colorsType={colorsType} selectedColor={selectedColor} setSelectedColor={setSelectedColor} />}
           <OrderTimeAndDate flexDirection='flex-col' />
           <AddToCartBtn />
           <CollectionInfo />
