de44c01eb2bf686df88c32fd5b004e146bfe3d7e
Johnny Tai <bm414148@gmail.com>
2023-07-31 16:42:05 +0800

    change src to  coverImage

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { IProduct } from './allProducts'
 import Link from 'next/link';
 
-export const ProductCard: React.FC<IProduct> = ({ id, category, name, width, height, src, price }) => {
+export const ProductCard: React.FC<IProduct> = ({ id, category, name, width, height, coverImage, src, price }) => {
   const formattedPrice = price.toLocaleString('zh-TW', {
     style: 'currency',
     currency: 'TWD',
@@ -14,7 +14,7 @@ export const ProductCard: React.FC<IProduct> = ({ id, category, name, width, hei
     <li key={id} className={`bg-[#f2f2f2] list-none overflow-hidden rounded-3xl border-4 border-solid border-white  flex flex-col justify-end ${height === 465 ? "basis-2/3" : "basis-1/3"}`} >
       <Link href={`/product/${id}`}>
         <div className="photo px-16 m-auto ">
-          <Image className="mx-auto mb-3.5 " src={src} width={width} height={height} alt={name} />
+          <Image className="mx-auto mb-3.5 " src={coverImage} width={width} height={height} alt={name} />
         </div>
         <div>
           <h3 className="text-center font-semibold text-lg mb-10 ">{name}</h3>
