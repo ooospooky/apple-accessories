558724c8251ea7127adf2c5ba4e25d539e499b8e
Johnny Tai <bm414148@gmail.com>
2023-08-01 14:30:01 +0800

    reduce duplication of code

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -9,50 +9,38 @@ interface CarouselPorps {
 export const Carousel: React.FC<CarouselPorps> = ({ src, selectedColor }) => {
   const [currentIndex, setCurrentIndex] = useState(0)
 
+  const handleChangeImage = (index: number) => {
+    setCurrentIndex(index);
+  };
+
+  const renderCarouselImages = (images: string[]) => (
+    <>
+      <div key={currentIndex} className='animate-fade animate-once animate-duration-[500ms]'>
+        <Image src={images[currentIndex]} layout="responsive" height={1} width={1} alt='' className='object-fill m-auto max-w-5xl max-h-5xl' />
+      </div>
+      <div className='w-full flex flex-row justify-center items-center'>
+        {images.map((image, index) => (
+          <Image
+            key={index}
+            src={image}
+            width={70}
+            height={70}
+            alt={`Thumbnail ${index + 1}`}
+            onClick={() => handleChangeImage(index)}
+            className={`${currentIndex === index ? 'pb-2 border-b-[3px] border-[#b1b1b2]' : ''}`}
+          />
+        ))}
+      </div>
+    </>
+  );
+
   if (Object.keys(src)[0] === 'noColor') {
-    return (
-      <>
-        <div key={currentIndex} className='animate-fade animate-once animate-duration-[500ms] '>
-          <Image src={src.noColor[currentIndex]} layout="responsive" height={1} width={1} alt='' className='object-fill m-auto max-w-5xl max-h-5xl' />
-        </div>
-        <div className=' w-full flex flex-row justify-center items-center'>
-          {src.noColor.map((image, index) => (
-            <Image
-              key={index}
-              src={image}
-              width={70}
-              height={70}
-              alt={`Thumbnail ${index + 1}`}
-              onClick={() => setCurrentIndex(index)}
-              className={`${currentIndex === index ? 'pb-2 border-b-[3px] border-[#b1b1b2]' : ""}`}
-            />
-          ))}
-        </div>
-      </>
-    )
+    return renderCarouselImages(src.noColor);
   }
-  if (selectedColor) {
-    return (
-      <>
-        <div key={currentIndex} className='animate-fade animate-once animate-duration-[500ms] '>
-          <Image src={src[selectedColor][currentIndex]} layout="responsive" height={1} width={1} alt='' className='object-fill m-auto max-w-5xl max-h-5xl ' />
-        </div>
-        <div className=' w-full flex flex-row justify-center items-center   '>
-          {src[selectedColor].map((image, index) => (
-            <Image
-              key={index}
-              src={image}
-              width={70}
-              height={70}
-              alt={`Thumbnail ${index + 1}`}
-              onClick={() => setCurrentIndex(index)}
-              className={`${currentIndex === index ? 'pb-2 border-b-[3px] border-[#b1b1b2]' : ""}`}
-            />
-          ))}
-        </div>
-      </>
-    )
-  } else {
-    return null;
+
+  if (selectedColor && src[selectedColor]) {
+    return renderCarouselImages(src[selectedColor]);
   }
+
+  return <></>;
 }
