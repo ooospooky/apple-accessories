b582b9c6a7037c1c42233b36a756724be58fe565
Johnny Tai <bm414148@gmail.com>
2023-08-01 19:24:38 +0800

    add redux action:addToCart & add handleAddToCart

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useState } from 'react'
 import { allProducts, IProduct } from '@/app/components/allProducts'
+import Link from 'next/link';
 import Image from 'next/image';
 import { OrderTimeAndDate } from '@/app/components/OrderTimeAndDate';
 import { AddToCartBtn } from '@/app/components/AddToCartBtn';
@@ -10,24 +11,33 @@ import { PriceInfo } from '@/app/components/PriceInfo';
 import { ProductInfoAndCompatibility } from '@/app/components/ProductInfoAndCompatibility';
 import { ColorSelect } from '@/app/components/ColorSelect';
 import { Carousel } from '@/app/components/Carousel';
-
+import { useAppDispatch, useAppSelector } from '@/redux/store';
+import { addToCart } from '@/redux/features/cartSlice';
 
 export default function ProductId({ params }: { params: { productId: string } }) {
   const [selectedColor, setSelectedColor] = useState<string | null>(null);
   const foundProduct = allProducts.find((item) => item.id === params.productId);
   const { id, category, name, width, height, coverImage, src, price, colorsType, colors, productInfo, compatibilityInfo }: IProduct = foundProduct || {} as IProduct;
 
+  const dispatch = useAppDispatch()
 
+  const handleAddToCart = () => {
+    dispatch(addToCart({ id: id, color: selectedColor ? selectedColor : 'noColor' }))
+  }
   return (
 
     <div className="w-full h-full   ">
+      <Link href="/cart">Cart</Link>
       <div className="w-7/12 h-full my-12 mx-auto flex flex-row">
         <div className="basis-1/3 flex flex-col gap-12">
           <h2 className="text-6xl font-semibold leading-tight ">{name} -{selectedColor}</h2>
           <PriceInfo price={price} />
           {colors && colorsType && <ColorSelect colors={colors} colorsType={colorsType} selectedColor={selectedColor} setSelectedColor={setSelectedColor} />}
           <OrderTimeAndDate flexDirection='flex-col' />
-          <AddToCartBtn />
+          {/* <AddToCartBtn /> */}
+          <div>
+            <button onClick={() => handleAddToCart()} className="inline-block w-full px-2 py-4 rounded-xl cursor-pointer text-center whitespace-no-wrap text-base font-normal bg-[#0071e3] hover:bg-[#0077ed] text-white  ">加入購物車</button>
+          </div>
           <CollectionInfo />
           <PurchaseAssistance />
 
