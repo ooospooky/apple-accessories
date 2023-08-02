6dfadb6d76da00a2aa628fcdeb751b24f875cc24
Johnny Tai <bm414148@gmail.com>
2023-08-02 15:45:40 +0800

    add w-1/2 when flexDirection === flex-row

--- src/app/components/OrderTimeAndDate.tsx
+++ src/app/components/OrderTimeAndDate.tsx
@@ -10,14 +10,14 @@ export const OrderTimeAndDate: React.FC<IorderTimeAndDate> = ({ flexDirection })
   return (
     // <>
     <div className={`flex ${flexDirection} gap-10`}>
-      <div className="flex flex-row gap-1">
+      <div className={`flex flex-row gap-1  ${flexDirection === 'flex-row' ? 'w-1/2' : ''}`}>
         <Image src="/svg/appleBag.svg" priority width={25} height={25} alt="appleBag" className='mr-3' />
         <div className="flex flex-col gap-1">
           <span className="text-xl font-normal">立即訂購。店內取貨：</span>
           <span className="text-xl font-normal"><span className="text-xl font-semibold">今天</span>，地點：Apple 台北 101</span>
         </div>
       </div>
-      <div className="flex flex-row gap-1">
+      <div className={`flex flex-row gap-1  ${flexDirection === 'flex-row' ? 'w-1/2' : ''}`}>
         <Image src="/svg/boxtruck.svg" priority width={25} height={25} alt="truck" className='mr-3' />
         <div className="flex flex-col gap-1">
           <span className="text-xl font-normal">今天訂購。 運送至您所在的地方</span>
