9f91e3b26ff4073e21c8c48f164590bbfe44e15f
Johnny Tai <bm414148@gmail.com>
2023-08-02 15:46:31 +0800

    add props for btn text

--- src/app/components/AddToCartBtn.tsx
+++ src/app/components/AddToCartBtn.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export const AddToCartBtn = () => {
+interface AddToCartBtnProps {
+  btnText: string;
+}
+
+export const AddToCartBtn: React.FC<AddToCartBtnProps> = ({ btnText }) => {
   return (
     <div>
-      <button className="inline-block w-full px-2 py-4 rounded-xl cursor-pointer text-center whitespace-no-wrap text-base font-normal bg-[#0071e3] hover:bg-[#0077ed] text-white  ">加入購物車</button>
+      <button className="inline-block w-full px-2 py-4 rounded-xl cursor-pointer text-center whitespace-no-wrap text-2xl font-normal bg-[#0071e3] hover:bg-[#0077ed] text-white  ">{btnText}</button>
     </div>
   )
 }
