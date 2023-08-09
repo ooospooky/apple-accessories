580e8ace9f41534d59840050cfe5dd440d7962a9
Johnny Tai <bm414148@gmail.com>
2023-08-09 15:31:42 +0800

    Navbar done

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -15,14 +15,16 @@ export const Navbar = () => {
 
   return (
     <nav className="bg-white border-gray-200">
-      <div className="max-w-screen-xl w-5/12 flex flex-wrap justify-evenly items-center mx-auto py-4 ">
-        <Link href="https://flowbite.com/" className="flex items-center">
+      <div className="max-w-screen-xl w-6/12 flex flex-wrap justify-evenly items-center mx-auto py-4 ">
+        <Link href="/" className="flex items-center md:grow-0 grow">
           <Image src='/svg/appleLogo.svg' priority width={14} height={44} alt="appleLogo" />
         </Link>
-
+        <Link href='/cart' className='md:order-last'>
+          <Image src='/svg/appleBag.svg' height={25} width={25} alt='' />
+        </Link>
         <button
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden"
           aria-controls="navbar-default"
           aria-expanded={isMenuOpen}
           onClick={handleMenuToggle}
@@ -32,33 +34,32 @@ export const Navbar = () => {
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
           </svg>
         </button>
-        <div className={`w-full md:block md:w-auto ${isMenuOpen ? '' : 'hidden '}`} id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8  md:mt-0 md:border-0 md:bg-white  dark:border-gray-700">
+        <div className={`w-full md:block  grow md:w-auto ${isMenuOpen ? '' : 'hidden '}`} id="navbar-default">
+          <ul className="font-medium flex flex-col pl-0 p-4 md:p-0 mt-4 rounded-lg md:flex-row md:justify-evenly  md:mt-0 md:border-0 md:bg-white ">
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-700 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-gray-700 md:dark:hover:bg-transparent">商店</a>
+              <Link href="/" className="block  py-2  pr-4 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-700 md:dark:hover:text-blue-500 md:dark:hover:bg-transparent">商店</Link>
+            </li>
+
+            <li>
+              <Link href="/mac" className="block py-2 pr-4 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-700 md:dark:hover:text-blue-500 md:dark:hover:bg-transparent">Mac</Link>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-700 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-gray-700 md:dark:hover:bg-transparent">Mac</a>
+              <Link href="/ipad" className="block py-2  pr-4 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-700 md:dark:hover:text-blue-500 md:dark:hover:bg-transparent">iPad</Link>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-700 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-gray-700 md:dark:hover:bg-transparent">Ipad</a>
+              <Link href="/iPhone" className="block py-2  pr-4 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-700 md:dark:hover:text-blue-500 md:dark:hover:bg-transparent">iPhone</Link>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-700 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-gray-700 md:dark:hover:bg-transparent">Iphone</a>
+              <Link href="/watch" className="block py-2  pr-4 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-700 md:dark:hover:text-blue-500 md:dark:hover:bg-transparent">Watch</Link>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-700 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-gray-700 md:dark:hover:bg-transparent">Watch</a>
-            </li>
-            <li>
-              <a href="https://support.apple.com/zh-tw" target='_blank' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-700 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-gray-700 md:dark:hover:bg-transparent">支援服務</a>
+              <a href="https://support.apple.com/zh-tw" target='_blank' className="block py-2  pr-4 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-700 md:dark:hover:text-blue-500 md:dark:hover:bg-transparent">支援服務</a>
             </li>
 
           </ul>
 
         </div>
-        <Link href='/cart' className=''>
-          <Image src='/svg/appleBag.svg' height={25} width={25} alt='' />
-        </Link>
+
       </div>
     </nav>
   );
