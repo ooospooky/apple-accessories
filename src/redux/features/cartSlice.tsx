487488d5559fbefa6b2126a0b98a2d0d86cc6057
Johnny Tai <bm414148@gmail.com>
2023-08-09 17:39:39 +0800

    add totalamount

--- src/redux/features/cartSlice.tsx
+++ src/redux/features/cartSlice.tsx
@@ -3,7 +3,8 @@ import type { PayloadAction } from '@reduxjs/toolkit'
 import { allProducts } from '@/app/components/allProducts'
 
 interface IinitialState {
-  value: IproductList
+  value: IproductList,
+  totalAmount: number
 }
 
 interface IproductList {
@@ -23,7 +24,7 @@ const getInitialState = () => {
   return productList;
 }
 
-let initialState: IinitialState = { value: getInitialState() }
+let initialState: IinitialState = { value: getInitialState(), totalAmount: 0 }
 
 
 export const cartSlice = createSlice({
@@ -38,12 +39,14 @@ export const cartSlice = createSlice({
       const { id, color } = action.payload;
       state.value[id][color] = (state.value[id][color] || 0) + 1;
       state.value[id].total += 1;
+      state.totalAmount += 1;
     },
     changeProductCount: ((state, action: PayloadAction<{ id: string; color: string; number: number; }>) => {
       const { id, color, number } = action.payload;
       let numberDiff = number - state.value[id][color]
       state.value[id][color] = state.value[id][color] + numberDiff;
       state.value[id].total += numberDiff;
+      state.totalAmount += numberDiff;
       // total:2 black:2
       // total:? black:7
     })
