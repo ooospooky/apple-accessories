b9962a2f6bcefa9f44239134bac447638bab685f
Johnny Tai <bm414148@gmail.com>
2023-08-12 17:47:33 +0800

    import axios

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect, ChangeEvent } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import axios from 'axios'
 import { useRouter } from 'next/navigation'
 import { useAppDispatch, useAppSelector } from '@/redux/store'
 
