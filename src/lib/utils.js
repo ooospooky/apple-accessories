faa778567100fdab5673000a2bf41aada71e6ec2
Johnny Tai <bm414148@gmail.com>
2023-08-20 13:15:11 +0800

    add fireworks animation

---
+++ src/lib/utils.js
@@ -0,0 +1,40 @@
+import confetti from "canvas-confetti";
+
+export const shootFireworks = () => {
+  console.log(123);
+  const duration = 3000;
+  const animationEnd = Date.now() + duration;
+  const defaults = {
+    startVelocity: 30,
+    spread: 360,
+    ticks: 60,
+    zIndex: 0,
+  };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const interval = setInterval(function () {
+    const timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    const particleCount = 50 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount: 70,
+        origin: { x: randomInRange(0.2, 0.4), y: Math.random() - 0.2 },
+      })
+    );
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount: 70,
+        origin: { x: randomInRange(0.6, 0.8), y: Math.random() - 0.2 },
+      })
+    );
+  }, 250);
+};
