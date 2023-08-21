1f2642272359c99c9ac5eab9e701b014fe58f2cf
Johnny Tai <bm414148@gmail.com>
2023-08-21 15:50:34 +0800

    add rwd

--- src/app/ipad/page.tsx
+++ src/app/ipad/page.tsx
@@ -4,12 +4,12 @@ import { ProductList } from '../components/ProductList'
 export default function Mac() {
   return (
     <div className='w-full'>
-      <div className='headblock w-full h-full flex flex-row bg-[#fbfbfd] px-80  '  >
+      <div className='headblock w-full h-full flex flex-col md:flex-row bg-[#fbfbfd] lg:px-20 xl:px-80  py-10'  >
         <div className="info basis-1/2 flex flex-col justify-center items-center  gap-12" >
           <h2 className="text-5xl">不只做更多，還做得漂亮。</h2>
           <p className='text-2xl'>選購保護配件與防護</p>
         </div>
-        <div className='basis-1/2 -ml-28' >
+        <div className='basis-1/2 xl:-ml-28' >
           <Image className=' ' src="/ipad/ipad-page.png" alt="Homepage-IMG" height={480} width={900} />
         </div>
       </div>
