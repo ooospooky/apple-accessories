fb6ccfd808aa416ab741bb66ccbad9614a6cfd3d
Johnny Tai <bm414148@gmail.com>
2023-08-27 13:40:13 +0800

    code optimization & add comment

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -4,14 +4,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useAppDispatch, useAppSelector } from '@/redux/store'
-
 import { changeProductCount, deleteFromCart } from '@/redux/features/cartSlice'
 import { allProducts, IProduct } from '../components/allProducts'
-import { AddToCartBtn } from '../components/AddToCartBtn'
 import getFormattedPrice from '../helper/getFormattedPrice'
 import { OrderTimeAndDate } from '../components/OrderTimeAndDate'
 import QuantityField from '../components/QuantityField'
-import { PurchaseAssistance } from '../components/PurchaseAssistance'
 import checkout from '../../api/checkout'
 import LoadingSvg from '@/app/components/loadingSvg';
 
@@ -36,11 +33,16 @@ export default function Cart() {
   // 在需要時更新 totalPrice
   useEffect(() => {
     const calculateTotalPrice = () => {
+      // 使用 reduce 函數來計算總價格
       return keys.reduce((total, id) => {
+
+        // 找到對應的產品
         const foundProduct = allProducts.find((item) => item.id === id);
+        // 如果產品存在且購物車中的數量大於 0，則加總到 total 中
         if (cart[id]['total'] > 0 && foundProduct) {
           return total + foundProduct.price * cart[id]['total'];
         }
+        // 否則保持原本的 total 值
         return total;
       }, 0);
     };
@@ -49,10 +51,13 @@ export default function Cart() {
 
   //更改商品數量邏輯
   const handleProductCountChange = (event: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>, id: string, color: string) => {
+    // 將用戶輸入的數量轉換成數字
     const count = Number(event.target.value)
-    //若用戶輸入數量小於1，只顯示最小值1
+
+    //確保數量不會小於 1，若用戶輸入數量小於1，只顯示最小值1
     const number: number = count < 1 ? 1 : count;
-    //更動redux狀態
+
+    //更動redux狀態，呼叫 changeProductCount action
     dispatch(changeProductCount({ id: id, color: color, number: number }))
 
   };
@@ -69,18 +74,24 @@ export default function Cart() {
   }
 
   const CartHeader = () => {
+    const cartItemsText = `以下是你購物袋內的商品 ${getFormattedPrice(totalPrice)}。`;
+    const shippingText = '所有訂單均享免額外付費運送服務。';
+
     return (
       <div className='w-full pb-16 border-b border-[#d2d2d7] text-center'>
-        <h1 className='block text-5xl md:text-6xl tracking-normal leading-normal font-semibold'>以下是你購物袋內的商品 {getFormattedPrice(totalPrice)}。</h1>
-        <span className='block text-2xl font-normal pt-10'>所有訂單均享免額外付費運送服務。</span>
-        <div className='mt-12 w-1/3 m-auto' >
+        <h1 className='block text-5xl md:text-6xl tracking-normal leading-normal font-semibold'>{cartItemsText}</h1>
+        <span className='block text-2xl font-normal pt-10'>{shippingText}</span>
+        <div className='mt-12 w-1/3 m-auto'>
           <button
             onClick={() => handleCashout()}
-            className="w-full inline-block mt-12 px-12 py-6 rounded-xl cursor-pointer text-center whitespace-no-wrap text-2xl font-normal bg-[#0071e3] hover:bg-[#0077ed] text-white  "> {isLoading ? <LoadingSvg /> : '結帳'}</button>
+            className="w-full inline-block mt-12 px-12 py-6 rounded-xl cursor-pointer text-center whitespace-no-wrap text-2xl font-normal bg-[#0071e3] hover:bg-[#0077ed] text-white"
+          >
+            {isLoading ? <LoadingSvg /> : '結帳'}
+          </button>
         </div>
       </div>
-    )
-  }
+    );
+  };
 
   interface NoColorOptionProductProps {
     total: number;
@@ -236,53 +247,44 @@ export default function Cart() {
     )
   }
 
-  const Summary = () => {
-    return (
-      <div className='w-9/12 flex flex-col ml-auto'>
-        <div className='w-full flex flex-row justify-between text-2xl mb-5'>
-          <span>小計</span>
-          <span>{getFormattedPrice(totalPrice)}</span>
-        </div>
-        <div className='w-full flex flex-row justify-between text-2xl'>
-          <span>運費</span>
-          <span>免額外付費</span>
-        </div>
-        <div className='w-full flex flex-row justify-between pt-9 mt-6 border-t border-[#d2d2d7] text-4xl font-semibold'>
-          <span>你的總金額</span>
-          <span>{getFormattedPrice(totalPrice)}</span>
-        </div>
-        <span className="text-xl text-[#6e6e73] text-right ">
-          含加值型營業稅{getFormattedPrice(Math.floor(totalPrice * 0.047))}
-        </span>
-        <span className="text-xl tracking-wide text-right mt-2 ">欲使用銀行轉帳付款，請致電 0800-020-021。</span>
+  const Summary = () => (
+    <div className='w-9/12 flex flex-col ml-auto'>
+      <div className='w-full flex flex-row justify-between text-2xl mb-5'>
+        <span>小計</span>
+        <span>{getFormattedPrice(totalPrice)}</span>
       </div>
-    )
-  }
-
-  const CashOut = () => {
-
-    return (
-      <div className='w-9/12 flex justify-end ml-auto'>
-
-        <button
-          onClick={() => handleCashout()}
-          className="w-1/2 inline-block mt-12 px-12 py-6 rounded-xl cursor-pointer text-center whitespace-no-wrap text-2xl font-normal bg-[#0071e3] hover:bg-[#0077ed] text-white  "> {isLoading ? <LoadingSvg /> : '結帳'}</button>
-
+      <div className='w-full flex flex-row justify-between text-2xl'>
+        <span>運費</span>
+        <span>免額外付費</span>
       </div>
-    )
-  }
-
-  const Assistance = () => {
-    return (
-      <div className='w-full my-24 py-12 border-y border-gray-400 flex flex-row items-center whitespace-nowrap text-2xl '>
-        <span className=" font-semibold">需要進一步協助？</span>
-        <a target="_blank" href="https://contactretail.apple.com/Help" className=" text-[#06c]">
-          立即與我們交流
-        </a>
-        <span className=" font-normal">或致電 0800-020-021。</span>
+      <div className='w-full flex flex-row justify-between pt-9 mt-6 border-t border-[#d2d2d7] text-4xl font-semibold'>
+        <span>你的總金額</span>
+        <span>{getFormattedPrice(totalPrice)}</span>
       </div>
-    )
-  }
+      <span className="text-xl text-[#6e6e73] text-right ">
+        含加值型營業稅{getFormattedPrice(Math.floor(totalPrice * 0.047))}
+      </span>
+      <span className="text-xl tracking-wide text-right mt-2 ">欲使用銀行轉帳付款，請致電 0800-020-021。</span>
+    </div>
+  )
+
+  const CashOut = () => (
+    <div className='w-9/12 flex justify-end ml-auto'>
+      <button
+        onClick={() => handleCashout()}
+        className="w-1/2 inline-block mt-12 px-12 py-6 rounded-xl cursor-pointer text-center whitespace-no-wrap text-2xl font-normal bg-[#0071e3] hover:bg-[#0077ed] text-white  "> {isLoading ? <LoadingSvg /> : '結帳'}</button>
+    </div>
+  )
+
+  const Assistance = () => (
+    <div className='w-full my-24 py-12 border-y border-gray-400 flex flex-row items-center whitespace-nowrap text-2xl '>
+      <span className=" font-semibold">需要進一步協助？</span>
+      <a target="_blank" href="https://contactretail.apple.com/Help" className=" text-[#06c]">
+        立即與我們交流
+      </a>
+      <span className=" font-normal">或致電 0800-020-021。</span>
+    </div>
+  )
 
   //購物車沒有商品時呈現其他畫面
   if (totalPrice <= 0) return (
